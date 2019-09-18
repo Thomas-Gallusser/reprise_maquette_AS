821a64b81f8baa15b4fd558eaf30c106bf40b233
Thomas-Gallusser <t.gallusser@codeur.online>
2019-09-18 09:26:44 +0200

    add jquery, prepare ajax to post product informations

--- functions.php
+++ functions.php
@@ -5,21 +5,27 @@ function add_product_menu() {
    add_menu_page("Take - Configuration","Ajouter un produit","manage_options", "takeadd_product", "add_product");
 }
 
+// Chargement du CSS de la page product
 wp_register_style('add-product-css', get_bloginfo('template_directory') . '/product.css');
 wp_enqueue_style('add-product-css');
 
+// Chargement du script de la page product
 wp_register_script('add-product-js', get_bloginfo('template_directory') . '/product.js');
 wp_enqueue_script('add-product-js');
+
+// Chargement de JQUERY
+wp_register_script('add-jquery-js', 'https://code.jquery.com/jquery-3.4.1.js');
+wp_enqueue_script('add-jquery-js');
 
 function add_product() {
    global $title;
 
    ?>
-   <div id="wrap" class="wrap">
+   <div id="wrap" class="wrap customCss">
      <!------------------------------------------------------------------------------------------>
-    <form class="" action="" method="post">
+    <form class="" action="test()" method="post">
       <input type='hidden' name='action' value='submitform'/>
-      <input type='submit' name='btn' value='test'/>
+      <input type='button' id='btn' name='btn' value='test' onClick='test()'/>
     </form>
      <!------------------------------------------------------------------------------------------>
    </div>
--- product.js
+++ product.js
@@ -1,1 +1,11 @@
-document.getElementById("wrap").innerHtml = "Test";
+function test() {
+  if (document.getElementById("btn").value == "test") {
+    document.getElementById("btn").value = "click";
+  } else {
+    document.getElementById("btn").value = "test";
+  }
+}
+
+function dah(){
+  $.ajax({type: 'POST', url: '../wp-content/themes/take/post_product.php', data: '', dataType: 'json'});
+}
